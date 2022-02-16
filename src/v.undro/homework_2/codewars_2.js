e4cdd20b6e6b59826a64784f4b19f2691b52221a
Vitaly Undro <vitalyundro@gmail.com>
2022-02-16 12:54:37 +0300

    correct the solution of the first kata and complete other kata

--- src/v.undro/homework_2/codewars_2.js
+++ src/v.undro/homework_2/codewars_2.js
@@ -1,7 +1,36 @@
 function isPalindrome(line) {
-  typeof line == "number" ? line = line.toString() : line = line;
-  let reversedLine = line.split("").reverse().join("");
-  return line == reversedLine;
+  return line.toString() == line.toString().split("").reverse().join("");
 }
 
-isPalindrome(889);
+// isPalindrome(889);
+
+function upArray(arr) {
+  if (arr.length === 0) return null;
+  if (arr.some((el) => typeof el !== "number" || el < 0 || el > 9)) return null;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] !== 9) {
+      arr[i]++;
+      break;
+    } else {
+      arr[i] = 0;
+    }
+
+    if (i === 0) {
+      arr.unshift(1);
+    }
+  }
+
+  return arr;
+}
+
+function pyramid(n) {
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    result.push(new Array(i + 1).fill(1));
+  }
+
+  return result;
+}
+// pyramid(5);
