a56a542811911981c769b1978d33629048dd489e
arseni <a.m.halilov@inbox.ru>
2022-02-16 11:30:10 +0300

    Homework2_2

--- src/a.halilov/Homework2/src/script.js
+++ src/a.halilov/Homework2/src/script.js
@@ -18,3 +18,26 @@ function repeatStr(n, str){
 }
 
 console.log(repeatStr(6, "I"));
+console.log(repeatStr(6, "Hello "));
+
+/* Второй вариант, не знаю, правильно или нет, но мне кажется так прикольно  */
+function repeatStrElse(n, str){
+    console.log(str.repeat(n));
+}
+repeatStrElse(6, "I");
+repeatStrElse(6, "Hello ");
+
+
+/* Задача 3 MaxMin*/
+
+function highAndLow(numbers){
+    numbers = numbers.split(" ");
+    return Math.max.apply(null, numbers) + "  " + Math.min.apply(null, numbers);
+
+}
+
+console.log(highAndLow("1 2 3 4 5 6 7 9"));
+console.log(highAndLow("1 2 -3 4 5"));
+console.log(highAndLow("1 9 3 4 -5"));
+
+
