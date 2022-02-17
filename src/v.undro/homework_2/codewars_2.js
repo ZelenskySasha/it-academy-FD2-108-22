72a437606015f49853f57b8aa34cc0293a3079be
Vitaly Undro <vitalyundro@gmail.com>
2022-02-17 09:05:19 +0300

    complete kata 4 and 5

--- src/v.undro/homework_2/codewars_2.js
+++ src/v.undro/homework_2/codewars_2.js
@@ -34,3 +34,27 @@ function pyramid(n) {
   return result;
 }
 // pyramid(5);
+
+function flatten(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result.push(...array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+// flatten([[[[3], [4], [5]], [9], [9], [8], [[1, 2, 3]]]]);
+
+function largestPairSum(numbers) {
+  let maxNumber1 = Math.max(...numbers);
+  numbers.splice(numbers.indexOf(Math.max(...numbers)), 1);
+  let maxNumber2 = Math.max(...numbers);
+
+  return maxNumber1 + maxNumber2;
+}
+
+// largestPairSum([-10, -8, -16, -18, -19]);
