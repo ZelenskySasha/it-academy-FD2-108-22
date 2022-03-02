3b4f9f3ed0411e3c58c4b4c83fd94ef651abe51f
arseni <a.m.halilov@inbox.ru>
2022-03-02 11:56:00 +0300

    home_4

---
+++ src/a.halilov/Home_4/from_lesson/script.js
@@ -0,0 +1,24 @@
+
+
+function generateTable(n){
+    let table = document.createElement("table");
+    
+    for(let i = 0; i<n; i++){
+        let tr = document.createElement("tr");
+        
+        for(let j = 0; j<n; j++){
+            let td = document.createElement("td");
+            tr.append(td);
+            let num1 = i + 1;
+            let num2 = j + 1;
+            td.textContent = num1 + " " + num2;
+            
+        }
+        table.append(tr);
+        
+    }
+    return table;
+
+}
+document.body.append(generateTable(5));
+
