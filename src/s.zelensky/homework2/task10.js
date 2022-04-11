9921256adcbf5d2d4139370ce7b31e6155abf1b9
sasha <zelensky.alexandr@gmail.com>
2022-04-11 23:43:37 +0300

    5 homework

--- src/s.zelensky/homework2/task10.js
+++
@@ -1,9 +0,0 @@
-function ReverseArray(number) {
-    let str = String(number);
-    let array = [];
-    for (i = 1; i <= str.length; i++) {
-        array.push(str[str.length - i]);
-    }
-    return array;
-}
-console.log(ReverseArray(348597));
--- src/s.zelensky/homework2/task9.js
+++
@@ -1,8 +0,0 @@
-function SquareSum(array) {
-    result = 0;
-    for (i = 0; i < array.length; i++) {
-        result += Math.pow(array[i], 2);
-    }
-    return result;
-}
-console.log(SquareSum([1, 2, 2]));
