e5eec2fcbd51893a881f84309ff4ae349eea172c
sasha <zelensky.alexandr@gmail.com>
2022-04-29 19:36:03 +0300

    Ещё не сделал

---
+++ src/s.zelensky/Homework_7/script.js
@@ -0,0 +1,15 @@
+function newGame() {
+
+}
+
+function createButtons() {}
+
+function createFrameForAllDiv() {
+    alert('d');
+    let headMenu = document.createElement('div');
+    //headMenu.id = 'headMenu';
+    document.getElementsByTagName("header").append(headMenu);
+    //table.getElementsByTagName("td");
+
+
+}
